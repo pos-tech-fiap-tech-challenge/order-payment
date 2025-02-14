a67236b396dd73b136ca360073bc64ed16ab58e5
Washington Oliveira <w_olv@hotmail.com>
2025-02-14 15:38:25 -0300

    remove configmaps do deployment

--- src/main/java/com/lanchonete/order_payment/adapters/entities/PaymentEntity.java
+++ src/main/java/com/lanchonete/order_payment/adapters/entities/PaymentEntity.java
@@ -3,8 +3,10 @@ package com.lanchonete.order_payment.adapters.entities;
 import com.lanchonete.order_payment.core.enums.PaymentGateway;
 import com.lanchonete.order_payment.core.enums.PaymentStatus;
 import com.lanchonete.order_payment.core.enums.PaymentType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,6 +15,8 @@ import java.util.UUID;
 @Document(collation = "payments")
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentEntity {
     @Id
     private String id;
