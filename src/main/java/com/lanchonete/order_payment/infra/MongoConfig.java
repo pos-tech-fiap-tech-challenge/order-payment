df1d6e91ab2c968c3f308ef4dbaa4503029a0b29
Washington Oliveira <w_olv@hotmail.com>
2025-02-14 22:50:02 -0300

    corrige deployment

--- src/main/java/com/lanchonete/order_payment/infra/MongoConfig.java
+++ src/main/java/com/lanchonete/order_payment/infra/MongoConfig.java
@@ -3,10 +3,10 @@ package com.lanchonete.order_payment.infra;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import javax.net.ssl.SSLContext;
+
+import javax.net.ssl.*;
 import java.io.FileInputStream;
 import java.security.KeyStore;
 import java.security.cert.CertificateFactory;
@@ -18,19 +18,29 @@ public class MongoConfig {
     @Bean
     public com.mongodb.client.MongoClient mongoClient() {
         try {
+            // Caminho correto do certificado no pod
             String certPath = "/certs/global-bundle.pem";
+
+            // Carregar o certificado do DocumentDB
             CertificateFactory factory = CertificateFactory.getInstance("X.509");
             FileInputStream certInputStream = new FileInputStream(certPath);
             X509Certificate caCert = (X509Certificate) factory.generateCertificate(certInputStream);
             certInputStream.close();
 
+            // Criar um KeyStore e adicionar o certificado
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             keyStore.load(null, null);
             keyStore.setCertificateEntry("caCert", caCert);
 
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, null, null);
+            // Criar um TrustManager que reconhece o certificado
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
 
+            // Criar contexto SSL com o TrustManager configurado
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+
+            // Configurar MongoDB para usar TLS corretamente
             MongoClientSettings settings = MongoClientSettings.builder()
                     .applyConnectionString(new ConnectionString("mongodb://lanchonete-instance.c56iugywe6gv.us-east-1.docdb.amazonaws.com:27017/order_payment?tls=true&retryWrites=false"))
                     .applyToSslSettings(builder -> builder.enabled(true).context(sslContext))
