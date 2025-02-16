6a26fea6514fc7eb06b4b3cb63b2f4e09f5f72d0
Washington Oliveira <w_olv@hotmail.com>
2025-02-16 12:26:59 -0300

    atualiza mongo config

--- src/main/java/com/lanchonete/order_payment/infra/MongoConfig.java
+++ src/main/java/com/lanchonete/order_payment/infra/MongoConfig.java
@@ -1,38 +1,32 @@
-package com.lanchonete.order_payment.infra;
-
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.net.ssl.SSLContext;
-import java.io.FileInputStream;
-import java.security.KeyStore;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-
-@Configuration
-public class MongoConfig {
-
+//package com.lanchonete.order_payment.infra;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import com.mongodb.ConnectionString;
+//import com.mongodb.MongoClientSettings;
+//import com.mongodb.client.MongoClients;
+//import com.mongodb.client.MongoClient;
+//
+//@Configuration
+//public class MongoConfig {
+//
+//    @Value("${SPRING_DATA_MONGODB_URI}")
+//    private String mongoUri;
+//
+//    @Value("${SPRING_DATA_MONGODB_USERNAME}")
+//    private String username;
+//
+//    @Value("${SPRING_DATA_MONGODB_PASSWORD}")
+//    private String password;
+//
 //    @Bean
 //    public MongoClient mongoClient() {
-//        try {
-//            // Definir o caminho do TrustStore
-//            System.setProperty("javax.net.ssl.trustStore", "/opt/java/openjdk/lib/security/cacerts");
-//            System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+//        String connectionString = String.format("mongodb+srv://%s:%s@%s",
+//                username, password, mongoUri.replace("mongodb+srv://", ""));
 //
-//            // Configurar conexão MongoDB com TLS
-//            String connectionString = "mongodb://root:password@lanchonete-instance.c56iugywe6gv.us-east-1.docdb.amazonaws.com:27017/order_payment?" +
-//                    "tls=true&retryWrites=false&replicaSet=rs0&readPreference=secondaryPreferred";
-//            MongoClientSettings settings = MongoClientSettings.builder()
-//                    .applyConnectionString(new ConnectionString(connectionString))
-//                    .build();
+//        System.out.println("Conectando ao MongoDB com URI: " + connectionString);
 //
-//            return MongoClients.create(settings);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Erro ao configurar conexão MongoDB com TLS", e);
-//        }
+//        return MongoClients.create(new ConnectionString(connectionString));
 //    }
-}
+//}
